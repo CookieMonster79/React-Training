8f8e84c2b65b140496c5019745b2705144ccd95e
Maxim Moskvitin <moskva-max@yandex.ru>
2026-03-04 10:08:40 +0300

    новая ветка

---
+++ mod2-react-components/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+//import './App.css'
+
+import Info, {MyButton} from './MyButton.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1>Привет, React!</h1>
+      <Info></Info>
+      <MyButton />
+      <MyButton />
+    </>
+  )
+}
+
+export default App
---
+++ mod2-react-components/my-react-app/src/MyButton.jsx
@@ -0,0 +1,9 @@
+export default function Info() {
+    return <p>мой файл с компонентом кнопка</p>
+}
+
+export function MyButton() {
+    return (
+        <button>Кнопка</button>
+    )
+}
